c1cd78a7ca3896f898247d492a15ee5c1c184a96
Srinath Tupil Muralidharan <srinath_muralidharan@apple.com>
2019-08-21 15:38:44 -0700

    [REVIEW] <rdar://problem/54267618> Navigation bar color does not match background color of step (#123)
    * respect background color override
    
    * setting navBarTintColor in InstructionStepVC
    
    * setting barTintColor in viewWillAppear

--- ResearchKit/Common/ORKInstructionStepViewController.m
+++ ResearchKit/Common/ORKInstructionStepViewController.m
@@ -131,6 +131,7 @@
 
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
+    [self.taskViewController.navigationBar setBarTintColor:self.view.backgroundColor];
 }
 
 - (void)viewDidLoad {
