f8f236ade5d3c2d00682aab556300a93c746fe79
Oscar San Juan <iOsj@apple.com>
2019-10-31 13:24:52 -0700

    Just to test upload

--- ci_postbuild.sh
+++ ci_postbuild.sh
@@ -6,38 +6,40 @@ fi
 
 pushd /tmp/ci/developmentexport
 
-if [ "${PIPELINE}" == "ORKCatalog" ]; then
-	if [ "${CI_BRANCH}" == "master" ]; then
-		BUNDLE_ID="com.example.researchkit-samplecode.ORKCatalog"
-		IPA_NAME="ORKCatalog.ipa"
-	elif [ "${CI_BRANCH}" == "stable" ]; then
-		BUNDLE_ID="com.example.researchkit-samplecode.ORKCatalog-qa"
-		IPA_NAME="ORKCatalog QA.ipa"
-	elif [ "${CI_BRANCH}" == "release/public" ]; then
-		BUNDLE_ID="com.example.researchkit-samplecode.ORKCatalog-public"
-		IPA_NAME="ORKCatalog Public.ipa"
-  elif [ "${CI_BRANCH}" == "release/internal" ]; then
-    BUNDLE_ID="com.example.researchkit-samplecode.ORKCatalog-internal"
-	else
-		echo "Only master, stable, or public-release will upload to appshack"
-		exit 0
-	fi
-elif [ "${PIPELINE}" == "ORKSample" ]; then
-	if [ "${CI_BRANCH}" == "master" ]; then
-		BUNDLE_ID="com.example.carekit-samplecode.ORKSample"
-		IPA_NAME="ORKSample.ipa"
-	elif [ "${CI_BRANCH}" == "stable" ]; then
-		BUNDLE_ID=""
-		IPA_NAME="ORKSample QA.ipa"
-	elif [ "${CI_BRANCH}" == "public-release" ]; then
-		BUNDLE_ID=""
-		IPA_NAME="ORKSample Public.ipa"
-	else
-		echo "Only master, stable, or public-release will upload to appshack"
-		exit 0
-	fi
-fi
+# if [ "${PIPELINE}" == "ORKCatalog" ]; then
+# 	if [ "${CI_BRANCH}" == "master" ]; then
+# 		BUNDLE_ID="com.example.researchkit-samplecode.ORKCatalog"
+# 		IPA_NAME="ORKCatalog.ipa"
+# 	elif [ "${CI_BRANCH}" == "stable" ]; then
+# 		BUNDLE_ID="com.example.researchkit-samplecode.ORKCatalog-qa"
+# 		IPA_NAME="ORKCatalog QA.ipa"
+# 	elif [ "${CI_BRANCH}" == "release/public" ]; then
+# 		BUNDLE_ID="com.example.researchkit-samplecode.ORKCatalog-public"
+# 		IPA_NAME="ORKCatalog Public.ipa"
+#   elif [ "${CI_BRANCH}" == "release/internal" ]; then
+#     BUNDLE_ID="com.example.researchkit-samplecode.ORKCatalog-internal"
+# 	else
+# 		echo "Only master, stable, or public-release will upload to appshack"
+# 		exit 0
+# 	fi
+# elif [ "${PIPELINE}" == "ORKSample" ]; then
+# 	if [ "${CI_BRANCH}" == "master" ]; then
+# 		BUNDLE_ID="com.example.carekit-samplecode.ORKSample"
+# 		IPA_NAME="ORKSample.ipa"
+# 	elif [ "${CI_BRANCH}" == "stable" ]; then
+# 		BUNDLE_ID=""
+# 		IPA_NAME="ORKSample QA.ipa"
+# 	elif [ "${CI_BRANCH}" == "public-release" ]; then
+# 		BUNDLE_ID=""
+# 		IPA_NAME="ORKSample Public.ipa"
+# 	else
+# 		echo "Only master, stable, or public-release will upload to appshack"
+# 		exit 0
+# 	fi
+# fi
 
+BUNDLE_ID="com.example.researchkit-samplecode.ORKCatalog"
+IPA_NAME="ORKCatalog.ipa"
 
 mkdir -p Payload
 rm -rf Payload/*
