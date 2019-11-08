04c47cabd0f32b652afb93d3702563372a4e2c0b
Pariece McKinney <pmckinney@apple.com>
2019-11-07 16:44:28 -0800

    set lineBreakMode to NSLineBreakByWordWrapping for title and description labels within the ORKStepContentView (#339)

--- ResearchKit/Common/ORKStepContentView.m
+++ ResearchKit/Common/ORKStepContentView.m
@@ -423,6 +423,7 @@ typedef NS_CLOSED_ENUM(NSInteger, ORKUpdateConstraintSequence) {
     }
     _textLabel.textAlignment = _stepHeaderTextAlignment;
     _textLabel.numberOfLines = 0;
+    _textLabel.lineBreakMode = NSLineBreakByWordWrapping;
     [self addSubview:_textLabel];
     [self setupTextLabelConstraints];
     [self setContainerLeftRightConstraints];
@@ -518,6 +519,7 @@ typedef NS_CLOSED_ENUM(NSInteger, ORKUpdateConstraintSequence) {
     }
     _detailTextLabel.textAlignment = _stepHeaderTextAlignment;
     _detailTextLabel.numberOfLines = 0;
+    _detailTextLabel.lineBreakMode = NSLineBreakByWordWrapping;
     [self addSubview:_detailTextLabel];
     [self setupDetailTextLabelConstraints];
     [self setContainerLeftRightConstraints];
