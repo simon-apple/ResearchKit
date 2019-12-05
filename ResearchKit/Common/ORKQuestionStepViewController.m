ecc2dd959dbef61e3aa53045aabb4f80f68a157e
Srinath Tupil Muralidharan <srinath_muralidharan@apple.com>
2019-12-05 11:09:17 -0800

    [REVIEW] <rdar://problem/57195473> ORK Catalog: Text Choice Question - keyboard obscures text entry into Other field (#367)
    * other cell text view is no longer blocked by keyboard when it appears
    
    * removed issue where cells position was set too high when the keyboard was presented
    
    * Removing 2nd textview delegate. Adding some initial maths to calculate inset needed.
    
    * Converting keyboard frame to view coordinates.
    
    * fix for other choice cell issue when presented in ORKFormStepViewController
    
    * clean up

--- ResearchKit/Common/ORKQuestionStepViewController.m
+++ ResearchKit/Common/ORKQuestionStepViewController.m
@@ -359,6 +359,8 @@ static const CGFloat DelayBeforeAutoScroll = 0.25;
     
     [self stepDidChange];
     
+    [NSNotificationCenter.defaultCenter addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
+    [NSNotificationCenter.defaultCenter addObserver:self selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
 }
 
 - (BOOL)showValidityAlertWithMessage:(NSString *)text {
@@ -625,6 +627,29 @@ static const CGFloat DelayBeforeAutoScroll = 0.25;
     // FIXME: - add explicit property in QuestionStep to dictate this behavior
 //    return [self.questionStep isFormatImmediateNavigation] && [self hasAnswer] == NO && !self.isBeingReviewed;
     return NO;
+}
+
+#pragma mark NSNotification methods
+
+- (void)keyboardWillShow:(NSNotification *)notification {
+    
+    CGRect keyboardFrame = [[[notification userInfo] objectForKey:UIKeyboardFrameEndUserInfoKey] CGRectValue];
+    CGRect convertedKeyboardFrame = [self.view convertRect:keyboardFrame fromView:nil];
+    
+    if (CGRectGetMaxY(_currentFirstResponderCell.frame) >= CGRectGetMinY(convertedKeyboardFrame)) {
+        
+        [self.tableView setContentInset:UIEdgeInsetsMake(0, 0, CGRectGetHeight(convertedKeyboardFrame) - CGRectGetHeight(_currentFirstResponderCell.frame), 0)];
+        
+        NSIndexPath *currentFirstResponderCellIndex = [self.tableView indexPathForCell:_currentFirstResponderCell];
+        
+        if (currentFirstResponderCellIndex) {
+            [self.tableView scrollToRowAtIndexPath:currentFirstResponderCellIndex atScrollPosition:UITableViewScrollPositionBottom animated:YES];
+        }
+    }
+}
+
+- (void)keyboardWillHide:(NSNotification *)notification {
+    [self.tableView setContentInset:UIEdgeInsetsZero];
 }
 
 #pragma mark - ORKQuestionStepCustomViewDelegate
