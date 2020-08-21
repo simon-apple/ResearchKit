3aafc67387d3f99c2a12d320a0c0c3bdc1ae32e1
Jason <j_bice@apple.com>
2020-08-21 12:49:21 -0700

    Setting up automation and scheme

---
+++ samples/ORKCatalog/ORKCatalogUITests/Helpers.swift
@@ -0,0 +1,9 @@
+//
+//  Helpers.swift
+//  ORKCatalogUITests
+//
+//  Created by Jason on 8/21/20.
+//  Copyright © 2020 researchkit.org. All rights reserved.
+//
+
+import Foundation
---
+++ samples/ORKCatalog/ORKCatalogUITests/Screens/TaskScreen.swift
@@ -0,0 +1,9 @@
+//
+//  TaskScreen.swift
+//  ORKCatalogUITests
+//
+//  Created by Jason on 8/21/20.
+//  Copyright © 2020 researchkit.org. All rights reserved.
+//
+
+import Foundation
