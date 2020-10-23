536767546e8809ec9b08f01e26a0c0c75861a529
Erik Hornberger <erik_h@apple.com>
2020-10-23 16:38:55 -0700

    Clean up test

--- ResearchKitTests/ORKVideoInstructionStepViewControllerTests.swift
+++ ResearchKitTests/ORKVideoInstructionStepViewControllerTests.swift
@@ -27,8 +27,7 @@
  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-import Darwin
-import ResearchKit.Private
+import ResearchKit
 import XCTest
 
 class ORKVideoInstructionStepViewControllerTests: XCTestCase {
