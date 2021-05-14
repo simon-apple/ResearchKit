9fc4e8ecd7a94c299399f2aad4ac08844af9d029
Pariece McKinney <pmckinney@apple.com>
2021-05-14 16:13:28 -0700

    fixed scroll issue by calling setPinNavigationContainer directly (#963)

--- ResearchKit/Common/ORKStepContainerView.m
+++ ResearchKit/Common/ORKStepContainerView.m
@@ -186,7 +186,7 @@ static NSString *scrollContentChangedNotification = @"scrollContentChanged";
         [self placeNavigationContainerView];
         _topContentImageShouldScroll = YES;
         _customContentTopPadding = ORKStepContainerTopCustomContentPaddingStandard;
-        _pinNavigationContainer = YES; // Default behavior is to pin the navigation footer
+        [self setPinNavigationContainer:YES]; // Default behavior is to pin the navigation footer
         [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(scrollContentChanged) name:scrollContentChangedNotification object:nil];
     }
     return self;
