11b16706979e93b6b0ab0142b8ae9bf20aec8b1a
Louie Chatta <lchatta@apple.com>
2023-07-20 15:29:04 -0700

    [REVIEW] rdar://110923554 ([ORKCatalog] Images turn into silhouettes in dark mode) (#1608)
    # Conflicts:
    #	ResearchKit.xcodeproj/project.pbxproj
    #	ResearchKit/Common/ORKImageSelectionView.m

--- ResearchKit/Common/ORKStepContentView.m
+++ ResearchKit/Common/ORKStepContentView.m
@@ -36,7 +36,7 @@
 #import "ORKCompletionCheckmarkView.h"
 #import "ORKBodyContainerView.h"
 #import "ORKSkin.h"
-
+#import "UIImageView+ResearchKit.h"
 
 /*
  +_________________________+
@@ -177,7 +177,27 @@ typedef NS_CLOSED_ENUM(NSInteger, ORKUpdateConstraintSequence) {
         _topContentImageView.image = [self topContentAndAuxiliaryImage];
     }
     
+    [self updateViewColors];
     [[NSNotificationCenter defaultCenter] postNotificationName:ORKStepTopContentImageChangedKey object:nil];
+}
+
+- (void)updateViewColors {
+    if (@available(iOS 12.0, *)) {
+        // [LC:NOTE] for dark mode we need to able to set the tint on the UIImageView,
+        // this requires the image to be set to the UIImageRenderingModeAlwaysTemplate rendering mode
+        // in the UIImageRenderingModeAlwaysOriginal rendering mode, tintColor does not apply
+        [_topContentImageView updateRenderingModeForUserInterfaceStyle:self.traitCollection.userInterfaceStyle];
+        [_iconImageView updateRenderingModeForUserInterfaceStyle:self.traitCollection.userInterfaceStyle];
+        
+        UIColor *imageViewTintColor = self.traitCollection.userInterfaceStyle == UIUserInterfaceStyleDark ? [UIColor whiteColor] : nil;
+        _topContentImageView.tintColor = imageViewTintColor;
+        _iconImageView.tintColor = imageViewTintColor;
+    }
+}
+
+- (void)traitCollectionDidChange:(UITraitCollection *)previousTraitCollection {
+    [super traitCollectionDidChange:previousTraitCollection];
+    [self updateViewColors];
 }
 
 - (void)setAuxiliaryImage:(UIImage *)auxiliaryImage {
@@ -299,6 +319,7 @@ typedef NS_CLOSED_ENUM(NSInteger, ORKUpdateConstraintSequence) {
     _iconImageView.contentMode = UIViewContentModeScaleAspectFit;
 
     [self addSubview:_iconImageView];
+    [self updateViewColors];
     [self setIconImageViewConstraints];
 }
 
