8a7e870b9e7c3939114ab525d1574643cdab13bf
Ricardo Sanchez Saez <rsanchezsaez@apple.com>
2023-09-05 10:38:23 -0700

    rdar://114883313 (Dawn Seed 8 Tools adoption) (#1686)
    [Jenkins] Update Xcode to Dawn Seed 8

--- JenkinsfileSDKs.groovy
+++ JenkinsfileSDKs.groovy
@@ -1,7 +1,7 @@
 def developmentSDK() {
-    return "RainbowSeed15A5195mDawnSeed21A5277gLighthouseSeed21R5305e"
+    return "_DawnSeed8_RainbowSeed15A5229mDawnSeed21A5325aLighthouseSeed21R5354a"
 }
 def xbsSDK() {
-    return "XIP_RainbowSeed15A5195m"
+    return "XIP_DawnSeed8_RainbowSeed15A5229m"
 }
 return this
