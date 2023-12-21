9c9698183da793a85c0a32a5b250d11387492c2f
Pariece McKinney <pmckinney@apple.com>
2023-12-21 17:55:37 -0500

    clean up

--- ResearchKitUI/ResearchKitUI_Private.h
+++ ResearchKitUI/ResearchKitUI_Private.h
@@ -28,33 +28,31 @@
  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#import <ResearchKitUI/ORKStepView_Private.h>
-#import <ResearchKitUI/ORKFormStepViewController_Private.h>
-#import <ResearchKitUI/ORKTaskViewController_Private.h>
-#import <ResearchKitUI/ORKTableContainerView.h>
-#import <ResearchKitUI/ORKStepContainerView_Private.h>
-#import <ResearchKitUI/ORKStepContentView.h>
-#import <ResearchKitUI/ORKStepViewController_Internal.h>
-#import <ResearchKitUI/ORKPasscodeStepViewController_Internal.h>
-
 #import <ResearchKitUI/ORKBodyContainerView.h>
+#import <ResearchKitUI/ORKCheckmarkView.h>
 #import <ResearchKitUI/ORKCustomStepView.h>
 #import <ResearchKitUI/ORKCustomStepView_Internal.h>
 #import <ResearchKitUI/ORKDirectionView.h>
+#import <ResearchKitUI/ORKFormStepViewController_Private.h>
 #import <ResearchKitUI/ORKFreehandDrawingView.h>
 #import <ResearchKitUI/ORKInstructionStepContainerView.h>
+#import <ResearchKitUI/ORKInstructionStepViewController_Internal.h>
 #import <ResearchKitUI/ORKLearnMoreView.h>
 #import <ResearchKitUI/ORKNavigationContainerView.h>
+#import <ResearchKitUI/ORKNavigationContainerView_Internal.h>
+#import <ResearchKitUI/ORKPasscodeStepViewController_Internal.h>
 #import <ResearchKitUI/ORKProgressView.h>
 #import <ResearchKitUI/ORKRingView.h>
 #import <ResearchKitUI/ORKSeparatorView.h>
+#import <ResearchKitUI/ORKStepContainerView_Private.h>
+#import <ResearchKitUI/ORKStepContentView.h>
 #import <ResearchKitUI/ORKStepHeaderView.h>
+#import <ResearchKitUI/ORKStepHeaderView_Internal.h>
+#import <ResearchKitUI/ORKStepViewController_Internal.h>
+#import <ResearchKitUI/ORKStepView_Private.h>
+#import <ResearchKitUI/ORKTableContainerView.h>
+#import <ResearchKitUI/ORKTaskViewController_Private.h>
+#import <ResearchKitUI/ORKTaskViewController_Internal.h>
 #import <ResearchKitUI/ORKTintedImageView.h>
 #import <ResearchKitUI/ORKVerticalContainerView.h>
 #import <ResearchKitUI/ORKViewControllerProviding.h>
-
-#import <ResearchKitUI/ORKCheckmarkView.h>
-#import <ResearchKitUI/ORKInstructionStepViewController_Internal.h>
-#import <ResearchKitUI/ORKNavigationContainerView_Internal.h>
-#import <ResearchKitUI/ORKStepHeaderView_Internal.h>
-#import <ResearchKitUI/ORKTaskViewController_Internal.h>
