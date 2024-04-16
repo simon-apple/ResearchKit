1ba09aad1648951d73259f43d8164d2ed08b0fd4
Simon Tsai <simon_tsai2@apple.com>
2024-04-16 14:40:16 -0700

    Captured signatures key in a constant.

--- ResearchKit/Stale/ORKConsentDocument.m
+++ ResearchKit/Stale/ORKConsentDocument.m
@@ -43,6 +43,8 @@
 #import "ORKErrors.h"
 #import "ORKSkin_Private.h"
 
+static NSString *const _ORKSignaturesKey = @"signatures";
+
 @implementation ORKConsentDocument {
     NSMutableArray<ORKConsentSignature *> *_signatures;
 }
@@ -233,7 +235,7 @@
         ORK_DECODE_OBJ_CLASS(aDecoder, signaturePageTitle, NSString);
         ORK_DECODE_OBJ_CLASS(aDecoder, signaturePageContent, NSString);
         ORK_DECODE_OBJ_CLASS(aDecoder, htmlReviewContent, NSString);
-        _signatures = [aDecoder decodeObjectOfClasses:[NSSet setWithArray:@[NSMutableArray.self, ORKConsentSignature.self]] forKey:@"signatures"];
+        _signatures = [aDecoder decodeObjectOfClasses:[NSSet setWithArray:@[NSMutableArray.self, ORKConsentSignature.self]] forKey:_ORKSignaturesKey];
         ORK_DECODE_OBJ_ARRAY(aDecoder, sections, ORKConsentSection);
     }
     return self;
