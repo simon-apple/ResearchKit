a92020cf862311dd804ef94bab4a1f1f0c1ede8d
Pariece McKinney <pmckinney@apple.com>
2024-06-20 16:50:50 -0400

    clean up

--- samples/ORKCatalog/ORKCatalog/ORKESerializer.h
+++ samples/ORKCatalog/ORKCatalog/ORKESerializer.h
@@ -28,6 +28,8 @@
  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+// apple-internal
+
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
--- samples/ORKCatalog/ORKCatalog/Tasks/TaskListRow.swift
+++ samples/ORKCatalog/ORKCatalog/Tasks/TaskListRow.swift
@@ -2303,27 +2303,25 @@ enum TaskListRow: Int, CustomStringConvertible {
     }
     
     private var readOnlyFormStepTask: ORKTask {
-        let instructionStep = TaskListRowSteps.readOnlyInstructionStepExample
-        let formStep = TaskListRowSteps.readOnlyFormStepExample
-        let heightQuestionStep = TaskListRowSteps.readOnlyHeightQuestionStepExample
-        let weightQuestionStep = TaskListRowSteps.readOnlyWeightQuestionStepExample
-        let textChoiceQuestionStep = TaskListRowSteps.readOnlyTextChoiceQuestionStepExample
-        let completionStep = TaskListRowSteps.completionStepExample
-        
         if let path = Bundle.main.path(forResource: "demographics_task", ofType: "json", inDirectory: "TaskExamples") {
             do {
                 let data = try Data(contentsOf: URL(fileURLWithPath: path))
                 let task = try ORKESerializer.object(fromJSONData: data) as? ORKNavigableOrderedTask
-                print("found the task!!")
+                
                 if let task = task {
                     return task
                 }
             } catch {
                 print("error while decoding task")
             }
-            
         }
         
+        let instructionStep = TaskListRowSteps.readOnlyInstructionStepExample
+        let formStep = TaskListRowSteps.readOnlyFormStepExample
+        let heightQuestionStep = TaskListRowSteps.readOnlyHeightQuestionStepExample
+        let weightQuestionStep = TaskListRowSteps.readOnlyWeightQuestionStepExample
+        let textChoiceQuestionStep = TaskListRowSteps.readOnlyTextChoiceQuestionStepExample
+        let completionStep = TaskListRowSteps.completionStepExample
         
         return ORKOrderedTask(identifier: String(describing: Identifier.readOnlyFormStepTask), steps: [instructionStep, formStep, heightQuestionStep, weightQuestionStep, textChoiceQuestionStep, completionStep])
     }
