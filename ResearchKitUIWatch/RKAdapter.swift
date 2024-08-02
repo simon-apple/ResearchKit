838d8dfb098e726ec19c4fe35a95f7d96b92f42b
Johnny Hicks <jd_hicks@apple.com>
2024-08-02 15:05:59 -0700

    DRY up the adapter with some primitive init

--- ResearchKitUIWatch/RKAdapter.swift
+++ ResearchKitUIWatch/RKAdapter.swift
@@ -34,290 +34,11 @@ import SwiftUI
 
 public class RKAdapter {
     public static func createFormRow(from item: ORKFormItem) -> FormRow? {
-        switch item.answerFormat {
-        case let textChoiceAnswerFormat as ORKTextChoiceAnswerFormat:
-            var answerOptions : [MultipleChoiceOption] = []
-            textChoiceAnswerFormat.textChoices.forEach { textChoice in
-                answerOptions.append(
-                    MultipleChoiceOption(
-                        id: UUID().uuidString,
-                        choiceText: textChoice.text
-                    )
-                )
-            }
-            return FormRow.multipleChoiceRow(
-                MultipleChoiceQuestion(
-                    id: item.identifier,
-                    title: item.text,
-                    choices: answerOptions,
-                    selectionType: textChoiceAnswerFormat.style == .singleChoice ? .single : .multiple
-                )
-            )
-        case let scaleAnswerFormat as ORKScaleAnswerFormat:
-            return FormRow.intSliderRow(
-                ScaleSliderQuestion(
-                    id: item.identifier,
-                    title: item.text ?? "",
-                    step: scaleAnswerFormat.step,
-                    range: scaleAnswerFormat.minimum...scaleAnswerFormat.maximum,
-                    value: scaleAnswerFormat.defaultValue
-                )
-            )
-        case let continuousScaleAnswerFormat as ORKContinuousScaleAnswerFormat:
-            
-            // Current ORKContinuousScaleAnswerFormat does not allow user to specify step size so we can create an approximation,
-            // falling back on 0.01 as our step size if required.
-            var stepSize = 0.01
-            let numberOfValues = continuousScaleAnswerFormat.maximum - continuousScaleAnswerFormat.minimum
-            if numberOfValues > 0 {
-                stepSize = 1.0 / numberOfValues
-            }
-            return FormRow.doubleSliderRow(
-                ScaleSliderQuestion(
-                    id: item.identifier,
-                    title: item.text ?? "",
-                    step: stepSize,
-                    range: continuousScaleAnswerFormat.minimum...continuousScaleAnswerFormat.maximum,
-                    value: continuousScaleAnswerFormat.defaultValue
-                )
-            )
-            
-        case let textChoiceScaleAnswerFormat as ORKTextScaleAnswerFormat:
-            let answerOptions = textChoiceScaleAnswerFormat.textChoices.map { textChoice in
-                MultipleChoiceOption(
-                    id: UUID().uuidString,
-                    choiceText: textChoice.text
-                )
-            }
-            guard var defaultOption = answerOptions.first else {
-                fatalError("Invalid Choice Array for ORKTextScaleAnswerFormat")
-            }
-            if answerOptions.indices.contains(textChoiceScaleAnswerFormat.defaultIndex) {
-                defaultOption = answerOptions[textChoiceScaleAnswerFormat.defaultIndex]
-            }
-            
-            return FormRow.textSliderStep(
-                ScaleSliderQuestion(
-                    id: item.identifier,
-                    title: item.text ?? "",
-                    options: answerOptions,
-                    selectedMultipleChoiceOption: defaultOption
-                )
-            )
-        case let textAnswerFormat as ORKTextAnswerFormat:
-            return FormRow.textRow(
-                TextQuestion(
-                    title: item.text ?? "",
-                    id: item.identifier,
-                    text: textAnswerFormat.defaultTextAnswer ?? "",
-                    prompt: textAnswerFormat.placeholder ?? "",
-                    textFieldType: textAnswerFormat.multipleLines ? .multiline : .singleLine,
-                    characterLimit: textAnswerFormat.maximumLength,
-                    hideCharacterCountLabel: textAnswerFormat.hideCharacterCountLabel,
-                    hideClearButton: textAnswerFormat.hideClearButton
-                )
-            )
-        case let dateTimeAnswerFormat as ORKDateAnswerFormat:
-            let prompt: String = {
-                if let placeholder = item.placeholder {
-                    return placeholder
-                }
-
-                if dateTimeAnswerFormat.style == .dateAndTime {
-                    return "Select Date and Time"
-                } else {
-                    return "Select Date"
-                }
-            }()
-
-            let startDate: Date = {
-                if let date = dateTimeAnswerFormat.minimumDate {
-                    return date
-                }
-                return Date.distantPast
-            }()
-
-            let endDate: Date = {
-                if let date = dateTimeAnswerFormat.maximumDate {
-                    return date
-                }
-                return Date.distantFuture
-            }()
-
-            let components: DatePicker.Components = {
-                switch dateTimeAnswerFormat.style {
-                case .date:
-                    return [.date]
-                case .dateAndTime:
-                    return [.date, .hourAndMinute]
-                default:
-                    return [.date]
-                }
-            }()
-
-            return FormRow.dateRow(
-                DateQuestion(
-                    id: item.identifier,
-                    title: item.text ?? "",
-                    selection: Date(),
-                    pickerPrompt: prompt,
-                    displayedComponents: components,
-                    range: startDate...endDate
-                )
-            )
-        case let numericAnswerFormat as ORKNumericAnswerFormat:
-            return FormRow.numericRow(
-                NumericQuestion(
-                    id: item.identifier,
-                    title: item.text ?? "",
-                    detail: item.detailText,
-                    prompt: numericAnswerFormat.placeholder ?? "Tap to answer",
-                    number: numericAnswerFormat.defaultNumericAnswer
-                )
-            )
-        case let heightAnswerFormat as ORKHeightAnswerFormat:
-            let measurementSystem: MeasurementSystem = {
-                switch heightAnswerFormat.measurementSystem {
-                case .USC:
-                    return .USC
-                case .local:
-                    return .local
-                case .metric:
-                    return .metric
-                @unknown default:
-                    return .metric
-                }
-            }()
-
-            let initialPrimaryValue: Int = {
-                // To set the picker at a nice middle of the road height
-                // we will set it to 5 feet initially
-                if measurementSystem == .USC {
-                    return 5
-                }
-
-                // Similar to above, this equate to 5'4" which
-                // is a good starting point for the picker.
-                if measurementSystem == .metric {
-                    return 162
-                }
-
-                return Locale.current.measurementSystem == .us ? 5 : 162
-            }()
-
-            return FormRow.heightRow(
-                HeightQuestion(
-                    id: item.identifier,
-                    title: item.text ?? "",
-                    detail: item.detailText,
-                    measurementSystem: measurementSystem,
-                    selection: (initialPrimaryValue, 4) // Denotes 4 inches which is paired with a 5 foot selection (162 cm)
-                )
-            )
-        case let weightAnswerFormat as ORKWeightAnswerFormat:
-            let measurementSystem: MeasurementSystem = {
-                switch weightAnswerFormat.measurementSystem {
-                case .USC:
-                    return .USC
-                case .local:
-                    return .local
-                case .metric:
-                    return .metric
-                @unknown default:
-                    return .metric
-                }
-            }()
-
-            let precision: NumericPrecision = {
-                switch weightAnswerFormat.numericPrecision {
-                case .default:
-                    return .default
-                case .high:
-                    return .high
-                case .low:
-                    return .low
-                }
-            }()
-
-            // At the moment the RK API for weight answer format defaults these values
-            // to the `greatestFiniteMagnitude` if you don't explicitly pass them in.
-            // We want to check for that here and pass in a valid value.
-            let defaultValue: Double? = {
-                if weightAnswerFormat.defaultValue == Double.greatestFiniteMagnitude {
-                    if measurementSystem == .USC {
-                        return 133
-                    } else {
-                        return 60
-                    }
-                }
-
-                return weightAnswerFormat.defaultValue
-            }()
-
-            let minimumValue: Double? = {
-                if weightAnswerFormat.minimumValue == Double.greatestFiniteMagnitude {
-                    return nil
-                }
-
-                return weightAnswerFormat.minimumValue
-            }()
-
-            let maximumValue: Double? = {
-                if weightAnswerFormat.maximumValue == Double.greatestFiniteMagnitude {
-                    return nil
-                }
-
-                return weightAnswerFormat.maximumValue
-            }()
-
-            return FormRow.weightRow(
-                WeightQuestion(
-                    id: item.identifier,
-                    title: item.text ?? "",
-                    detail: item.text,
-                    measurementSystem: measurementSystem,
-                    precision: precision,
-                    defaultValue: defaultValue,
-                    minimumValue: minimumValue,
-                    maximumValue: maximumValue,
-                    selection: (defaultValue, 0)
-                )
-            )
-        case let imageChoiceAnswerFormat as ORKImageChoiceAnswerFormat:
-            let choices = imageChoiceAnswerFormat.imageChoices.map { choice in
-                let value = (choice.value as? NSNumber) as! Int
-                return ImageChoice(
-                    id: UUID(),
-                    normalImage: choice.normalStateImage,
-                    selectedImage: choice.selectedStateImage,
-                    text: choice.text!, value: value
-                )
-            }
-
-            let style: ImageChoiceQuestion.ChoiceSelectionType = {
-                switch imageChoiceAnswerFormat.style {
-                    case .singleChoice:
-                    return .single
-                case .multipleChoice:
-                    return .multiple
-                default: return .single
-                }
-            }()
-
-            return FormRow.imageRow(
-                ImageChoiceQuestion(
-                    title: item.text ?? "",
-                    detail: item.detailText,
-                    id: item.identifier,
-                    choices: choices,
-                    style: style,
-                    vertical: imageChoiceAnswerFormat.isVertical,
-                    selections: []
-                )
-            )
-        default:
+        guard let answerFormat = item.answerFormat else {
             return nil
         }
+
+        return Self.createFormRow(from: item.identifier, with: item.text ?? "", detail: item.detailText, placeholder: item.placeholder, answer: answerFormat)
     }
 
     public static func createFormRow(from step: ORKStep, for answer: ORKAnswerFormat) -> FormRow? {
@@ -328,6 +49,20 @@ public class RKAdapter {
                 return step.text
             }
         }()
+
+        let placeholder: String? = {
+            if let formStep = step as? ORKFormStep,
+               let placeholder = formStep.formItems?.first?.placeholder {
+                return placeholder
+            }
+            return nil
+        }()
+
+        return Self.createFormRow(from: step.identifier, with: itemText ?? "", detail: step.detailText, placeholder: placeholder, answer: answer)
+    }
+
+    public static func createFormRow(from identifier: String, with title: String, detail: String?, placeholder: String? = nil, answer: ORKAnswerFormat) -> FormRow? {
+
         switch answer {
         case let textChoiceAnswerFormat as ORKTextChoiceAnswerFormat:
             var answerOptions : [MultipleChoiceOption] = []
@@ -341,8 +76,8 @@ public class RKAdapter {
             }
             return FormRow.multipleChoiceRow(
                 MultipleChoiceQuestion(
-                    id: step.identifier,
-                    title: itemText,
+                    id: identifier,
+                    title: title,
                     choices: answerOptions,
                     selectionType: textChoiceAnswerFormat.style == .singleChoice ? .single : .multiple
                 )
@@ -350,8 +85,8 @@ public class RKAdapter {
         case let scaleAnswerFormat as ORKScaleAnswerFormat:
             return FormRow.intSliderRow(
                 ScaleSliderQuestion(
-                    id: step.identifier,
-                    title: itemText ?? "",
+                    id: identifier,
+                    title: title,
                     step: scaleAnswerFormat.step,
                     range: scaleAnswerFormat.minimum...scaleAnswerFormat.maximum,
                     value: scaleAnswerFormat.defaultValue
@@ -368,8 +103,8 @@ public class RKAdapter {
             }
             return FormRow.doubleSliderRow(
                 ScaleSliderQuestion(
-                    id: step.identifier,
-                    title: itemText ?? "",
+                    id: identifier,
+                    title: title,
                     step: stepSize,
                     range: continuousScaleAnswerFormat.minimum...continuousScaleAnswerFormat.maximum,
                     value: continuousScaleAnswerFormat.defaultValue
@@ -392,8 +127,8 @@ public class RKAdapter {
 
             return FormRow.textSliderStep(
                 ScaleSliderQuestion(
-                    id: step.identifier,
-                    title: itemText ?? "",
+                    id: identifier,
+                    title: title,
                     options: answerOptions,
                     selectedMultipleChoiceOption: defaultOption
                 )
@@ -401,10 +136,10 @@ public class RKAdapter {
         case let textAnswerFormat as ORKTextAnswerFormat:
             return FormRow.textRow(
                 TextQuestion(
-                    title: itemText ?? "",
-                    id: step.identifier,
+                    title: title,
+                    id: identifier,
                     text: textAnswerFormat.defaultTextAnswer ?? "",
-                    prompt: textAnswerFormat.placeholder ?? "",
+                    prompt: placeholder ?? "",
                     textFieldType: textAnswerFormat.multipleLines ? .multiline : .singleLine,
                     characterLimit: textAnswerFormat.maximumLength,
                     hideCharacterCountLabel: textAnswerFormat.hideCharacterCountLabel,
@@ -413,8 +148,7 @@ public class RKAdapter {
             )
         case let dateTimeAnswerFormat as ORKDateAnswerFormat:
             let prompt: String = {
-                if let formStep = step as? ORKFormStep,
-                   let placeholder = formStep.formItems?.first?.placeholder {
+                if let placeholder {
                     return placeholder
                 }
 
@@ -452,8 +186,8 @@ public class RKAdapter {
 
             return FormRow.dateRow(
                 DateQuestion(
-                    id: step.identifier,
-                    title: itemText ?? "",
+                    id: identifier,
+                    title: title,
                     selection: Date(),
                     pickerPrompt: prompt,
                     displayedComponents: components,
@@ -463,9 +197,9 @@ public class RKAdapter {
         case let numericAnswerFormat as ORKNumericAnswerFormat:
             return FormRow.numericRow(
                 NumericQuestion(
-                    id: step.identifier,
-                    title: itemText ?? "",
-                    detail: step.detailText,
+                    id: identifier,
+                    title: title,
+                    detail: detail,
                     prompt: numericAnswerFormat.placeholder ?? "Tap to answer",
                     number: numericAnswerFormat.defaultNumericAnswer
                 )
@@ -502,9 +236,9 @@ public class RKAdapter {
 
             return FormRow.heightRow(
                 HeightQuestion(
-                    id: step.identifier,
-                    title: itemText ?? "",
-                    detail: step.detailText,
+                    id: identifier,
+                    title: title,
+                    detail: detail,
                     measurementSystem: measurementSystem,
                     selection: (initialPrimaryValue, 4) // Denotes 4 inches which is paired with a 5 foot selection (162 cm)
                 )
@@ -567,9 +301,9 @@ public class RKAdapter {
 
             return FormRow.weightRow(
                 WeightQuestion(
-                    id: step.identifier,
-                    title: itemText ?? "",
-                    detail: step.detailText,
+                    id: identifier,
+                    title: title,
+                    detail: detail,
                     measurementSystem: measurementSystem,
                     precision: precision,
                     defaultValue: defaultValue,
@@ -601,9 +335,9 @@ public class RKAdapter {
 
             return FormRow.imageRow(
                 ImageChoiceQuestion(
-                    title: itemText ?? "",
-                    detail: step.detailText,
-                    id: step.identifier,
+                    title: title,
+                    detail: detail,
+                    id: identifier,
                     choices: choices,
                     style: style,
                     vertical: imageChoiceAnswerFormat.isVertical,
@@ -677,12 +411,15 @@ public class RKAdapter {
             }
 
             if hasMatchingIdentifiers(firstIdentifier: item.identifier, secondIdentifier: builder?.identifier ?? "") {
+                let newFormItem = ORKFormItem(
+                    identifier: item.identifier,
+                    text: builder?.text,
+                    answerFormat: item.answerFormat
+                )
+                newFormItem.placeholder = item.placeholder
+
                 groupedItems.append(
-                    ORKFormItem(
-                        identifier: item.identifier,
-                        text: builder?.text,
-                        answerFormat: item.answerFormat
-                    )
+                    newFormItem
                 )
                 builder = nil
             } else {
