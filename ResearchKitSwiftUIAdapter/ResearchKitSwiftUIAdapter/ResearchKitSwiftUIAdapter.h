8568d47d5eb98d9cb86653031fee5440ab9cd9e4
Simon Tsai <simon_tsai2@apple.com>
2024-08-28 16:29:44 -0700

    Added new project for ResearchKitSwiftUIAdapter.

---
+++ ResearchKitSwiftUIAdapter/ResearchKitSwiftUIAdapter/ResearchKitSwiftUIAdapter.h
@@ -0,0 +1,18 @@
+//
+//  ResearchKitSwiftUIAdapter.h
+//  ResearchKitSwiftUIAdapter
+//
+//  Created by Simon Tsai on 8/28/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ResearchKitSwiftUIAdapter.
+FOUNDATION_EXPORT double ResearchKitSwiftUIAdapterVersionNumber;
+
+//! Project version string for ResearchKitSwiftUIAdapter.
+FOUNDATION_EXPORT const unsigned char ResearchKitSwiftUIAdapterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ResearchKitSwiftUIAdapter/PublicHeader.h>
+
+
---
+++ ResearchKitSwiftUIAdapter/ResearchKitSwiftUIAdapterTests/ResearchKitSwiftUIAdapterTests.swift
@@ -0,0 +1,17 @@
+//
+//  ResearchKitSwiftUIAdapterTests.swift
+//  ResearchKitSwiftUIAdapterTests
+//
+//  Created by Simon Tsai on 8/28/24.
+//
+
+import Testing
+@testable import ResearchKitSwiftUIAdapter
+
+struct ResearchKitSwiftUIAdapterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
