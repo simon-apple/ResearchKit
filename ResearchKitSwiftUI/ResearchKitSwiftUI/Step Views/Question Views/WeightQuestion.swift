80761d64490232d23c9180a585f4cfd484249fdb
Simon Tsai <simon_tsai2@apple.com>
2024-10-08 15:33:44 -0700

    Don’t need this function.

--- ResearchKitSwiftUI/ResearchKitSwiftUI/Step Views/Question Views/WeightQuestion.swift
+++ ResearchKitSwiftUI/ResearchKitSwiftUI/Step Views/Question Views/WeightQuestion.swift
@@ -561,11 +561,6 @@ struct WeightPickerView: View {
 }
 
 private extension Decimal {
-    
-    mutating func round(_ scale: Int, _ roundingMode: NSDecimalNumber.RoundingMode) {
-        var localCopy = self
-        NSDecimalRound(&self, &localCopy, scale, roundingMode)
-    }
 
     func rounded(_ scale: Int, _ roundingMode: NSDecimalNumber.RoundingMode) -> Decimal {
         var result = Decimal()
