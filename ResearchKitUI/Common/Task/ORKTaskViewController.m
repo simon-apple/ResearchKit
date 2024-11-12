5ed43cfa23a96ce0cd7675f78af7b41db1113ef9
Simon Tsai <simon_tsai2@apple.com>
2024-11-12 10:23:22 -0800

    Fixed issue where responses were lost for resumed tasks.

--- ResearchKitUI/Common/Task/ORKTaskViewController.m
+++ ResearchKitUI/Common/Task/ORKTaskViewController.m
@@ -722,13 +722,24 @@ static NSString *const _ChildNavigationControllerRestorationKey = @"childNavigat
 
 - (NSArray *)managedResultsArray {
     NSMutableArray *results = [NSMutableArray new];
-    
-    [_managedStepIdentifiers enumerateObjectsUsingBlock:^(NSString *identifier, NSUInteger idx, BOOL *stop) {
-        id <NSCopying> key = identifier;
-        ORKResult *result = _managedResults[key];
-        NSAssert2(result, @"Result should not be nil for identifier %@ with key %@", identifier, key);
-        [results addObject:result];
-    }];
+        
+    if ([_task isKindOfClass:[ORKOrderedTask class]]) {
+        NSLog(@"--- Is ORKOrderedTask");
+        ORKOrderedTask *orderedTask = (ORKOrderedTask *)_task;
+        for (ORKStep *step in orderedTask.steps) {
+            ORKResult *result = _managedResults[step.identifier];
+            if (result) {
+                [results addObject:result];
+            }
+        }
+    } else {
+        [_managedStepIdentifiers enumerateObjectsUsingBlock:^(NSString *identifier, NSUInteger idx, BOOL *stop) {
+            id <NSCopying> key = identifier;
+            ORKResult *result = _managedResults[key];
+            NSAssert2(result, @"Result should not be nil for identifier %@ with key %@", identifier, key);
+            [results addObject:result];
+        }];
+    }
     
     return [results copy];
 }
@@ -1492,6 +1503,7 @@ static NSString *const _ChildNavigationControllerRestorationKey = @"childNavigat
             // Remove the identifier from the list
             assert([itemId isEqualToString:_managedStepIdentifiers.lastObject]);
             [_managedStepIdentifiers removeLastObject];
+            NSLog(@"--- Removed last step identifier (1)");
             
             [self showStepViewController:stepViewController goForward:NO animated:animated];
         }
